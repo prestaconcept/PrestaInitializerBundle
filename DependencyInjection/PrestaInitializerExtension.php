d337a985f0986d51f689b21a62b0e0c7a76e54a1
Nicolas Bastien <nbastien@prestaconcept.net>
2014-09-25 10:57:27 +0200

    Remove default generated code

--- DependencyInjection/PrestaInitializerExtension.php
+++ DependencyInjection/PrestaInitializerExtension.php
@@ -28,6 +28,5 @@ class PrestaInitializerExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
     }
 }
